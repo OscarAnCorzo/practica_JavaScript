805e6f513ed7113ebfa87d8a075ae9d25c4d9422
oscancor117 <oscarcorzog@gmail.com>
2019-09-01 22:53:58 -0500

    evento jquery con texto

--- jQuery/02-textos.js
+++ jQuery/02-textos.js
@@ -1,8 +1,19 @@
 $(document).ready(function(){
     console.log($('a').length);
+    // recorre todo las etiquetas a y añade el texto con el contenido del link
+    $('a').each(function(index){
+        var texto = $(this).attr('href');
+        $(this).text(texto);
+    });
 
     
-
-    
+    $('#registrar').click(function(){
+        $('ul').append('<li><a href="'+$('#texto').val()+'"></a></li>');
+        $('#texto').val('');
+        $('a').each(function(index){
+            var texto = $(this).attr('href');
+            $(this).text(texto);
+        });
+    });
 
 });
